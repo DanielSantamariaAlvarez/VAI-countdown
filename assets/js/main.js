7d10fe31865598a3da6a50ebd941b7ace86fb094
DanielSantamariaAlvarez <47115911+DanielSantamariaAlvarez@users.noreply.github.com>
2022-06-17 10:59:29 -0500

    Version 1.0

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+let modal = document.getElementById('modal')
+let btnForm = document.getElementById('btn-form')
+let btnClose = document.getElementById('close')
+
+let formualrio = () =>{
+    modal.classList.toggle('active')
+}
+
+btnForm.addEventListener('click', formualrio)
+btnClose.addEventListener('click', formualrio);
+
+
+
+//===
+// VARIABLES
+//===
+const DATE_TARGET = new Date('10/01/2022 0:00 AM');
+// DOM for render
+const SPAN_DAYS = document.querySelector('#dias');
+const SPAN_HOURS = document.querySelector('#horas');
+const SPAN_MINUTES = document.querySelector('#minutos');
+const SPAN_SECONDS = document.querySelector('#segundos');
+// Milliseconds for the calculations
+const MILLISECONDS_OF_A_SECOND = 1000;
+const MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
+const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
+const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24
+
+//===
+// FUNCTIONS
+//===
+
+/**
+ * Method that updates the countdown and the sample
+ */
+function updateCountdown() {
+    // Calcs
+    const NOW = new Date()
+    const DURATION = DATE_TARGET - NOW;
+    const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
+    const REMAINING_HOURS = Math.floor((DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR);
+    const REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE);
+    const REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND);
+
+    // Render
+    SPAN_DAYS.textContent = REMAINING_DAYS;
+    SPAN_HOURS.textContent = REMAINING_HOURS;
+    SPAN_MINUTES.textContent = REMAINING_MINUTES;
+    SPAN_SECONDS.textContent = REMAINING_SECONDS;
+}
+
+//===
+// INIT
+//===
+updateCountdown();
+// Refresh every second
+setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
